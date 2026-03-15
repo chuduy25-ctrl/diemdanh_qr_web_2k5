9621b870587eb696f68c0a78bfc31fbd0d0f8dd8
chuduy25-ctrl <chuduy23032005@gmail.com>
2026-03-15 07:16:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+﻿let gheDaChon = null;
+
+function chonGhe(num) {
+
+    gheDaChon = num;
+
+    let seats = document.querySelectorAll(".seat");
+
+    seats.forEach(function (seat) {
+        seat.classList.remove("selected");
+    });
+
+    seats[num - 1].classList.add("selected");
+
+}
+
+function submitForm() {
+
+    let mssv = document.getElementById("mssv").value;
+
+    if (mssv == "" || gheDaChon == null) {
+        alert("Vui lòng nhập MSSV và chọn ghế");
+        return;
+    }
+
+    alert("MSSV: " + mssv + " chọn ghế: " + gheDaChon);
+
+}
